5822863673c17c51e165dfbdb69b863e8bdc636e
Mistkeithy <108419207+Mistkeithy@users.noreply.github.com>
2024-07-04 18:10:59 +0300

    Fixed download error on element-web-update.sh

--- element-web-update.sh
+++ element-web-update.sh
@@ -4,14 +4,14 @@
 # Script for check new version of Element from GitHub
 # and download new version, if update is avaiable
 #
-# https://github.com/MurzNN/element-web-update
+# https://github.com/Mistkeithy/element-web-update
 #
 ###################################################################
 
 # You can override those variables using .env file in your directory
 
 # Directory where Element files must be placed
-DIRECTORY_INSTALL=~/public_html
+DIRECTORY_INSTALL=/www/wwwroot/matrix.icu.su
 
 # Directory for temp files - must be different than install directory!
 DIRECTORY_TMP=/tmp
@@ -22,7 +22,7 @@ VERSION_URL=https://api.github.com/repos/vector-im/element-web/releases/latest
 if [ -f ".env" ]; then source .env; fi
 
 VERSION_INSTALLED=`if [ -f "$DIRECTORY_INSTALL/version" ]; then cat $DIRECTORY_INSTALL/version; else echo "null"; fi`
-VERSION_LATEST=`curl -s $VERSION_URL | jq -r '.name' | sed s/v//` || { echo "Error checking last Element version!"; exit 1; }
+VERSION_LATEST=`curl -sL $VERSION_URL | jq -r '.name' | sed s/v//` || { echo "Error checking last Element version!"; exit 1; }
 
 command -v curl >/dev/null 2>&1 || { echo "You need to install "curl" package for this script: sudo apt install curl"; exit 1; }
 command -v tar >/dev/null 2>&1 || { echo "You need to install "tar" package for this script: sudo apt install tar"; exit 1; }
@@ -30,12 +30,14 @@ command -v jq >/dev/null 2>&1 || { echo "You need to install "jq" package for th
 
 if ( [[ -z "$VERSION_LATEST" ]] || [ "$VERSION_LATEST" == "null" ] ); then
   echo "Error! Received bad version number from $VERSION_URL: $VERSION_LATEST"
-  exit
+  echo "Full response from GitHub API:"
+  curl -sL $VERSION_URL
+  exit 1
 fi
 
 if ( [ "$VERSION_INSTALLED" != "$VERSION_LATEST" ] ); then
   echo "Element installed version is $VERSION_INSTALLED, in GitHub releases found fresher version: $VERSION_LATEST - updating..."
-  DL_URL=`curl -s $VERSION_URL | jq -r '.assets[0].browser_download_url'`
+  DL_URL=`curl -sL $VERSION_URL | jq -r '.assets[0].browser_download_url'`
   curl -L -o $DIRECTORY_TMP/element-latest.tar.gz $DL_URL || { echo "Error downloading element-latest.tar.gz"; exit 1; }
   mkdir $DIRECTORY_TMP/element-latest/
   tar -xf $DIRECTORY_TMP/element-latest.tar.gz --strip 1 -C $DIRECTORY_TMP/element-latest/
